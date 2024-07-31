2b837f4aeed8637a26a5514962802c90a4aa2c7b
Anton Balanin <anton.balanin08@gmail.com>
2024-07-31 20:40:18 +0200

    added different bmi messages

--- bmi calculator/scripts/bmi.js
+++ bmi calculator/scripts/bmi.js
@@ -5,7 +5,20 @@ function calculateBMI() {
     const weight = document.querySelector('.bmi-input-weight')
         .value
 
-
     const bmi = weight / Math.pow(height, 2)
-    console.log(bmi)
+    console.log(bmi.toFixed(1))
+
+    let result = ''
+
+    if (bmi.toFixed(1) < 18.5 && bmi.toFixed(1) > 0) {
+        result = 'Untergewicht'
+    } else if (bmi.toFixed(1) > 18.5 && bmi.toFixed(1) < 24.9) {
+        result = 'Normalgewicht'
+    } else {
+        result = 'Übergewicht'
+    }
+
+    document.querySelector('.js-div')
+        .innerHTML = `Ihr BMI ist ${bmi}.
+      Sie haben ${result}`
 }
